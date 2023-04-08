6cf983de0695cbe833efd6e7945c1a89ecb16650
IMS212 <ims@irisshaders.dev>
2023-04-08 08:00:45 -0700

    Update screen

--- shaders/lib/iris_required.glsl
+++ shaders/lib/iris_required.glsl
@@ -128,180 +128,187 @@ const ivec2 spaceSize = charSize + ivec2(charSpacing, lineSpacing);
 // Text renderer
 
 struct Text {
-	vec4 result;     // Output color from the text renderer
-	vec4 fgCol;      // Text foreground color
-	vec4 bgCol;      // Text background color
-	ivec2 fragPos;   // The position of the fragment (can be scaled to adjust the size of the text)
-	ivec2 textPos;   // The position of the top-left corner of the text
-	ivec2 charPos;   // The position of the next character in the text
-	int base;        // Number base
-	int fpPrecision; // Number of decimal places to print
+    vec4 result;     // Output color from the text renderer
+    vec4 fgCol;      // Text foreground color
+    vec4 bgCol;      // Text background color
+    ivec2 fragPos;   // The position of the fragment (can be scaled to adjust the size of the text)
+    ivec2 textPos;   // The position of the top-left corner of the text
+    ivec2 charPos;   // The position of the next character in the text
+    int base;        // Number base
+    int fpPrecision; // Number of decimal places to print
 } text;
 
 // Fills the global text object with default values
 void beginText(ivec2 fragPos, ivec2 textPos) {
-	text.result      = vec4(0.0);
-	text.fgCol       = vec4(1.0);
-	text.bgCol       = vec4(0.0, 0.0, 0.0, 0.6);
-	text.fragPos     = fragPos;
-	text.textPos     = textPos;
-	text.charPos     = ivec2(0);
-	text.base        = 10;
-	text.fpPrecision = 2;
+    text.result      = vec4(0.0);
+    text.fgCol       = vec4(1.0);
+    text.bgCol       = vec4(0.0, 0.0, 0.0, 0.6);
+    text.fragPos     = fragPos;
+    text.textPos     = textPos;
+    text.charPos     = ivec2(0);
+    text.base        = 10;
+    text.fpPrecision = 2;
 }
 
 // Applies the rendered text to the fragment
 void endText(inout vec3 fragColor) {
-	fragColor = mix(fragColor.rgb, text.result.rgb, text.result.a);
+    fragColor = mix(fragColor.rgb, text.result.rgb, text.result.a);
 }
 
 void printChar(uint character) {
-	ivec2 pos = text.fragPos - text.textPos - spaceSize * text.charPos * ivec2(1, -1) + ivec2(0, spaceSize.y);
+    ivec2 pos = text.fragPos - text.textPos - spaceSize * text.charPos * ivec2(1, -1) + ivec2(0, spaceSize.y);
 
-	uint index = uint(charWidth - pos.x + pos.y * charWidth + 1);
+    uint index = uint(charWidth - pos.x + pos.y * charWidth + 1);
 
-	// Draw background
-	if (clamp(pos, ivec2(0), spaceSize - 1) == pos)
-		text.result = mix(text.result, text.bgCol, text.bgCol.a);
+    // Draw background
+    if (clamp(pos, ivec2(0), spaceSize - 1) == pos)
+    text.result = mix(text.result, text.bgCol, text.bgCol.a);
 
-	// Draw character
-	if (clamp(pos, ivec2(0), charSize - 1) == pos)
-		text.result = mix(text.result, text.fgCol, text.fgCol.a * float(character >> index & 1u));
+    // Draw character
+    if (clamp(pos, ivec2(0), charSize - 1) == pos)
+    text.result = mix(text.result, text.fgCol, text.fgCol.a * float(character >> index & 1u));
 
-	// Advance to next character
-	text.charPos.x++;
+    // Advance to next character
+    text.charPos.x++;
 }
 
 #define printString(string) {                                               \
-	uint[] characters = uint[] string;                                     \
-	for (int i = 0; i < characters.length(); ++i) printChar(characters[i]); \
+uint[] characters = uint[] string;                                     \
+for (int i = 0; i < characters.length(); ++i) printChar(characters[i]); \
 }
 
 
 
 // https://www.shadertoy.com/view/4tXyWN
 uint hash(uvec2 x) {
-    uvec2 q = 1103515245U * ((x >> 1U) ^ x.yx);
-    uint n = 1103515245U * (q.x ^ (q.y >> 3U));
-    return n;
+uvec2 q = 1103515245U * ((x >> 1U) ^ x.yx);
+uint n = 1103515245U * (q.x ^ (q.y >> 3U));
+return n;
 }
 
 // https://github.com/riccardoscalco/glsl-pcg-prng
 uint pcg(inout uint state) {
-	uint newState = state * uint(747796405) + uint(2891336453);
-	uint word = ((newState >> ((newState >> uint(28)) + uint(4))) ^ newState) * uint(277803737);
-	state = (word >> uint(22)) ^ word;
-    return state;
+uint newState = state * uint(747796405) + uint(2891336453);
+uint word = ((newState >> ((newState >> uint(28)) + uint(4))) ^ newState) * uint(277803737);
+state = (word >> uint(22)) ^ word;
+return state;
 }
 
 float rand(inout uint state) {
-	return float(pcg(state)) / float(uint(0xffffffff));
+return float(pcg(state)) / float(uint(0xffffffff));
 }
 
 void drawStars(ivec2 cellIndex, vec2 cellUV, inout vec3 color) {
-    uint state = hash(uvec2(cellIndex));
-    if (rand(state) > STAR_PERCENTAGE) {
-        // Not a star
-        return;
-    }
+uint state = hash(uvec2(cellIndex));
+if (rand(state) > STAR_PERCENTAGE) {
+// Not a star
+return;
+}
 
-    cellUV = (cellUV - 0.5) * 1.5 + 0.5;
-    if (clamp(cellUV, 0.0, 1.0) != cellUV)
-        return;
+cellUV = (cellUV - 0.5) * 1.5 + 0.5;
+if (clamp(cellUV, 0.0, 1.0) != cellUV)
+return;
 
-    float direction = rand(state) < 0.5 ? -1.0 : 1.0;
-    float offset = rand(state) * float(STAR_STATES);
-    int starState = (int(floor(offset + frameTimeCounter * STAR_SHINE_SPEED * direction)) % STAR_STATES + STAR_STATES) % STAR_STATES;
+float direction = rand(state) < 0.5 ? -1.0 : 1.0;
+float offset = rand(state) * float(STAR_STATES);
+int starState = (int(floor(offset + frameTimeCounter * STAR_SHINE_SPEED * direction)) % STAR_STATES + STAR_STATES) % STAR_STATES;
 
-    ivec2 pixel = abs(ivec2(cellUV * 7.0) - ivec2(3));
-    int pixelIndex = pixel.y * 4 + pixel.x;
-    int on = (STARS[starState] >> pixelIndex) & 1;
-    if (on == 0)
-        return;
+ivec2 pixel = abs(ivec2(cellUV * 7.0) - ivec2(3));
+int pixelIndex = pixel.y * 4 + pixel.x;
+int on = (STARS[starState] >> pixelIndex) & 1;
+if (on == 0)
+return;
 
-    color = vec3(0.4, 0.6, 0.9);
+color = vec3(0.4, 0.6, 0.9);
 }
 
 void drawRainbow(vec2 uv, inout vec3 color) {
-    if (uv.x > 0.5)
-        return;
+if (uv.x > 0.5)
+return;
 
-    uv.y -= viewHeight / viewWidth / 2.0; // Center
-    uv = floor(uv / PIXEL_SIZE);
-    float timeOffset = floor(frameTimeCounter * RAINBOW_WAVE_SPEED * 2.0);
-    uv.y += floor(mod(timeOffset + uv.x / WAVE_PIXEL_LENGTH, 2.0)) + 9.0; // Wave
-    if (clamp(uv.y, 0.0, 18.0) != uv.y)
-        return;
+uv.y -= viewHeight / viewWidth / 2.0; // Center
+uv = floor(uv / PIXEL_SIZE);
+float timeOffset = floor(frameTimeCounter * RAINBOW_WAVE_SPEED * 2.0);
+uv.y += floor(mod(timeOffset + uv.x / WAVE_PIXEL_LENGTH, 2.0)) + 9.0; // Wave
+if (clamp(uv.y, 0.0, 18.0) != uv.y)
+return;
 
-    int colorIndex = int(floor(uv.y / 3.0));
+int colorIndex = int(floor(uv.y / 3.0));
 
-    color = RAINBOW[colorIndex];
+color = RAINBOW[colorIndex];
 }
 
 void drawLogo(vec2 uv, inout vec3 color) {
-    vec2 aspect = vec2(viewWidth, viewHeight) / viewWidth;
-    uv = (uv - aspect / 2.0) / LOGO_SIZE + 0.5;
-    uv.x -= 0.3;
-    if (clamp(uv, 0.0, 1.0) != uv)
-        return;
+vec2 aspect = vec2(viewWidth, viewHeight) / viewWidth;
+uv = (uv - aspect / 2.0) / LOGO_SIZE + 0.5;
+uv.x -= 0.3;
+if (clamp(uv, 0.0, 1.0) != uv)
+return;
 
-    uv = floor(uv / PIXEL_SIZE / 3.0) * PIXEL_SIZE * 3.0;
+uv = floor(uv / PIXEL_SIZE / 3.0) * PIXEL_SIZE * 3.0;
 
-    uv -= 0.5;
-    float len = length(uv);
-    float angle = atan(uv.y, uv.x) - PI + frameTimeCounter;
-    float leafIndex = angle / 2.0 / PI * 16.0;
-    float func = abs(fract(leafIndex) - 0.5);
+uv -= 0.5;
+float len = length(uv);
+float angle = atan(uv.y, uv.x) - PI + frameTimeCounter;
+float leafIndex = angle / 2.0 / PI * 16.0;
+float func = abs(fract(leafIndex) - 0.5);
 
-    float outerRadius = (1.0 - pow(func, 0.8)) * 0.15 + 0.35;
-    if (len > outerRadius)
-        return;
+float outerRadius = (1.0 - pow(func, 0.8)) * 0.15 + 0.35;
+if (len > outerRadius)
+return;
 
-    float innerRadius = pow(func, 1.2) * 0.08 + 0.18;
-    if (len < innerRadius)
-        return;
+float innerRadius = pow(func, 1.2) * 0.08 + 0.18;
+if (len < innerRadius)
+return;
 
-    int colorIndex = int(floor(fract(angle / 2.0 / PI - pow((len - 0.35) / 0.5, 2.0) * 0.3) * 16.0));
+int colorIndex = int(floor(fract(angle / 2.0 / PI - pow((len - 0.35) / 0.5, 2.0) * 0.3) * 16.0));
 
-    color = LOGO_COLORS[colorIndex];
-    if (len > outerRadius - 0.05)
-        color *= 0.8;
+color = LOGO_COLORS[colorIndex];
+if (len > outerRadius - 0.05)
+color *= 0.8;
 }
 
 void main() {
-    vec2 uv = gl_FragCoord.xy / viewWidth;
+vec2 uv = gl_FragCoord.xy / viewWidth;
 
-    vec2 starUV = (uv + frameTimeCounter * SPACE_SPEED) / STAR_CELL_SIZE;
-    ivec2 cellIndex = ivec2(floor(starUV));
-    vec2 cellUV = fract(starUV);
+vec2 starUV = (uv + frameTimeCounter * SPACE_SPEED) / STAR_CELL_SIZE;
+ivec2 cellIndex = ivec2(floor(starUV));
+vec2 cellUV = fract(starUV);
 
-    vec3 color = SPACE_COLOR;
-    float pixelSize = viewWidth / 200.0;
-    vec2 coord = gl_FragCoord.xy / pixelSize;
-    drawStars(cellIndex, cellUV, color);
-    drawRainbow(uv, color);
-    drawLogo(uv, color);
+vec3 color = SPACE_COLOR;
+float pixelSize = viewWidth / 200.0;
+vec2 coord = gl_FragCoord.xy / pixelSize;
+drawStars(cellIndex, cellUV, color);
+drawRainbow(uv, color);
+drawLogo(uv, color);
 
-    // trick: use if defined instead of ifdef to not make it a shader option
-    #if defined OLD
+#if MC_VERSION < 11802
+    beginText(ivec2(coord), ivec2(viewWidth / pixelSize / 2.0 - 13.0 * 7.0, 24.0));
+text.bgCol = vec4(0.0);
+text.fgCol = vec4(LOGO_COLORS[int(coord.x / 6.0 + frameTimeCounter * 4.0) % 16], 1.0);
+printString((_U, _p, _g, _r, _a, _d, _e, _space, _M, _i, _n, _e, _c, _r, _a, _f, _t));
+endText(color);
+
+// trick: use if defined instead of ifdef to not make it a shader option
+#elif defined OLD
             beginText(ivec2(coord), ivec2(viewWidth / pixelSize / 2.0 - 13.0 * 7.0, 24.0));
-                text.bgCol = vec4(0.0);
-                text.fgCol = vec4(LOGO_COLORS[int(coord.x / 6.0 + frameTimeCounter * 4.0) % 16], 1.0);
-    printString((_U, _p, _g, _r, _a, _d, _e, _space, _I, _r, _i, _s, _space, _t, _o, _space, _u, _s, _e, _space, _t, _h, _i, _s, _space, _s, _h, _a, _d, _e, _r));
-        endText(color);
-    #else
+text.bgCol = vec4(0.0);
+text.fgCol = vec4(LOGO_COLORS[int(coord.x / 6.0 + frameTimeCounter * 4.0) % 16], 1.0);
+printString((_U, _p, _g, _r, _a, _d, _e, _space, _I, _r, _i, _s, _space, _t, _o, _space, _u, _s, _e, _space, _t, _h, _i, _s, _space, _s, _h, _a, _d, _e, _r));
+endText(color);
+#else
         beginText(ivec2(coord), ivec2(viewWidth / pixelSize / 2.0 - 13.0 * 6.0, 110.0));
-    text.bgCol = vec4(0.0);
-    text.fgCol = vec4(LOGO_COLORS[int(coord.x / 6.0 + frameTimeCounter * 4.0) % 16], 1.0);
-    printString((_T, _h, _i, _s, _space, _s, _h, _a, _d, _e, _r, _space, _r, _e, _q, _u, _i, _r, _e, _s, _space, _I, _r, _i, _s, _colon))
-    endText(color);
-    beginText(ivec2(coord), ivec2(viewWidth / pixelSize / 2.0 - 7.5 * 6.0, 20.0));
-    text.bgCol = vec4(0.0);
-    text.fgCol = vec4(LOGO_COLORS[int(coord.x / 6.0 + frameTimeCounter * 4.0) % 16], 1.0);
-    printString((_i, _r, _i, _s, _s, _h, _a, _d, _e, _r, _s, _dot, _n, _e, _t));
-    endText(color);
+text.bgCol = vec4(0.0);
+text.fgCol = vec4(LOGO_COLORS[int(coord.x / 6.0 + frameTimeCounter * 4.0) % 16], 1.0);
+printString((_T, _h, _i, _s, _space, _s, _h, _a, _d, _e, _r, _space, _r, _e, _q, _u, _i, _r, _e, _s, _space, _I, _r, _i, _s, _colon))
+endText(color);
+beginText(ivec2(coord), ivec2(viewWidth / pixelSize / 2.0 - 7.5 * 6.0, 20.0));
+text.bgCol = vec4(0.0);
+text.fgCol = vec4(LOGO_COLORS[int(coord.x / 6.0 + frameTimeCounter * 4.0) % 16], 1.0);
+printString((_i, _r, _i, _s, _s, _h, _a, _d, _e, _r, _s, _dot, _n, _e, _t));
+endText(color);
 
-    #endif
+#endif
 
     gl_FragData[0] = vec4(color, 1.0);
 }
